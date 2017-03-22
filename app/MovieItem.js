26756aa23131a378bec69c100cb7f28963aba845
Kacper Grzeszczyk <kacgrzes@gmail.com>
2017-03-22 14:43:50 +0100

    Refactor MovieItem component

--- app/MovieItem.js
+++ app/MovieItem.js
@@ -7,20 +7,19 @@ import {
   StyleSheet
 } from 'react-native';
 
-const MovieItem = ({title, poster_path, vote_average, onPress}) => {
-  return <TouchableOpacity onPress={onPress}>
-      <View style={styles.container}>
-        <View style={styles.imageContainer}>
-          <Image source={{uri: `https://image.tmdb.org/t/p/w92/${poster_path}`}}
-                 style={{width: 50, height: 50}} />
-        </View>
-        <View style={styles.textContainer}>
-          <Text>{title}</Text>
-          <Text>Score: {vote_average}</Text>
-        </View>
+const MovieItem = ({title, poster_path, vote_average, onPress}) =>
+  <TouchableOpacity onPress={onPress}>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={{uri: `https://image.tmdb.org/t/p/w92/${poster_path}`}}
+               style={{width: 50, height: 50}} />
       </View>
-    </TouchableOpacity>;
-};
+      <View style={styles.textContainer}>
+        <Text>{title}</Text>
+        <Text>Score: {vote_average}</Text>
+      </View>
+    </View>
+  </TouchableOpacity>;
 
 const styles = StyleSheet.create({
   container: {
